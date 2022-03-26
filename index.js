f07ff6ab6959a51ee91b82e3bdcdbb8571d363d6
Christ Kamanda <kamandachristo@gmail.com>
2022-03-26 14:08:59 -0400

    Prework JS Advanced Functions First-Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {return drivers.slice(0,2)}
+const returnLastTwoDrivers = function(drivers) {return drivers.slice(-2)}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+createFareMultiplier = (int) => {return (fare) =>{return int*fare};}
+function fareDoubler(fare){return fare * 2}
+function fareTripler(fare){return fare * 3}
+
+
+
+function selectDifferentDrivers(arrayOfDrivers, callBack){
+    if (callBack === returnFirstTwoDrivers || callBack === returnLastTwoDrivers) {
+        return callBack(arrayOfDrivers)
+    }
+
+
+}
+
+
+
+
+//function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers){
+  //  returnLastTwoDrivers = [,`Amari`, `Mo`];
+    //return returnLastTwoDrivers
+
+//}
+
